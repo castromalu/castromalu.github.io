515d7f77f665427cb8bbea5fc036b13963461494
castromalu <50372299+castromalu@users.noreply.github.com>
2019-05-27 23:09:42 -0300

    Add files via upload

---
+++ MariaLuizaJoguinho/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1559007337,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/personagprincip-sheet0.png",
+		"images/botãoptiro-sheet0.png",
+		"images/botãoptiro-sheet1.png",
+		"images/bala-sheet0.png",
+		"images/parabaixo-sheet0.png",
+		"images/parabaixo-sheet1.png",
+		"images/paracima-sheet0.png",
+		"images/paracima-sheet1.png",
+		"images/paraesquerda-sheet0.png",
+		"images/paraesquerda-sheet1.png",
+		"images/paradireita-sheet0.png",
+		"images/paradireita-sheet1.png",
+		"images/fundo-sheet0.png",
+		"images/fundo-sheet1.png",
+		"images/inimigo-sheet0.png",
+		"images/botondstroy-sheet0.png",
+		"images/vida-sheet0.png",
+		"images/vidaa-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
